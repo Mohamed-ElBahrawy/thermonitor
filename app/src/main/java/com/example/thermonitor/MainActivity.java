8f171be1193e7218be0e59abf36b2c1df9901109
lenovo <ba7rawy98@gmail.com>
2019-02-14 19:40:19 +0200

    Thermonitor

--- app/src/main/java/com/example/thermonitor/MainActivity.java
+++ app/src/main/java/com/example/thermonitor/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.thermonitor;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
 
    private EditText E_Mail;
    private EditText Password;
    private Button Login;
+   private TextView Account;
+   private Button SignUp;
+   private FirebaseAuth firebaseAuth;
+
 
    @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +37,49 @@ public class MainActivity extends AppCompatActivity {
         E_Mail=(EditText)findViewById(R.id.etMail);
         Password=(EditText)findViewById(R.id.etPassword);
         Login=(Button)findViewById(R.id.etLogin);
+       SignUp=(Button)findViewById(R.id.etSignup);
+       Account=(TextView)findViewById(R.id.etAccount);
+       firebaseAuth=FirebaseAuth.getInstance();
+
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSecondActivity();
+                if(TextUtils.isEmpty(E_Mail.getText().toString())||(TextUtils.isEmpty(Password.getText().toString()))){
+                    Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    validate(E_Mail.getText().toString(), Password.getText().toString());
+                }
+
 
             }
         });
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFourthActivity();
+            }
+        });
    }
-   public void openSecondActivity(){
-       Intent intent= new Intent(MainActivity.this,SecondActivity.class);
-       startActivity(intent);
+
+    private void openFourthActivity() {
+        Intent intent1= new Intent(MainActivity.this,FourthActivity.class);
+        startActivity(intent1);
+    }
+
+   private void validate(final String username, final String userpassword){
+       firebaseAuth.signInWithEmailAndPassword(username,userpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+           @Override
+           public void onComplete(@NonNull Task<AuthResult> task) {
+               if(task.isSuccessful()){
+                   startActivity(new Intent(MainActivity.this,SecondActivity.class));
+               }else{
+                   Toast.makeText(MainActivity.this,"Login failed",Toast.LENGTH_SHORT).show();
+               }
+
+           }
+       });
+
    }
 }
--- app/src/main/java/com/example/thermonitor/SecondActivity.java
+++ app/src/main/java/com/example/thermonitor/SecondActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -15,23 +18,27 @@ import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
 
+    int[]Images={R.drawable.sql,R.drawable.javascript,R.drawable.java,R.drawable.javac,R.drawable.python,R.drawable.javacc};
+    String[] Names = {"SQL","JAVA","JAVA SCRIPT","C#","PYTHON","C++"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        ListView listView =(ListView)findViewById(R.id.list);
-        String[] fruits = {"apple","guava","pineapple","watermelon"};
-
-
-        ListAdapter myadapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,fruits);
-        listView.setAdapter(myadapter);
+        ListView listView =(ListView) findViewById(R.id.list);
+        CustomListView customListView=new CustomListView(this,Names,Images);
+        listView.setAdapter(customListView);
 
        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
            @Override
            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-               Intent intent= new Intent(SecondActivity.this,ThirdActivity.class);
-               startActivity(intent);
+               Intent intent3= new Intent(SecondActivity.this,ThirdActivity.class);
+               startActivity(intent3);
            }
        });
+
     }
+
+
+
 }
